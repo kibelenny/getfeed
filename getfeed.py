af55036b7c4cc8e4cdc2563f43b9161d31b1b428
leetrix17 <wlenny20@gmail.com>
2022-01-28 16:32:03 +0300

    Create getfeed.py
    This is a simple script to get atom/RSS feed from a site(s). The class Feed is used to get every link. It expects a link as input. Feed.get_feed will return check for both RSS and atom feed and give the output as a tuple, with missing feed returning none. Feed.get_rss will return only the RSS feed link, while Feed.get_atom. For both Feed.get_rss and Feed.get_atom, if the link is missing it returns None.

---
+++ getfeed.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+
+class Feed:
+
+    def __init__(self, link) -> None:
+        self.headers = {
+            'User-Agent': 'Chrome/97.0.4692.71'
+        }
+        try:
+            self.request = requests.get(link, headers=headers)
+        except Exception as e:
+            print('Error fetching the URL: ', link)
+            print(e)
+        try:
+            self.soup = BeautifulSoup(request.text, 'html.parser')
+        except Exception as e:
+            print('Could not parse the xml: ', self.url)
+            print(e)
+            
+    def get_rss(self):
+        rss_get = self.soup.find('link', type='application/rss+xml')
+        if rss_get:
+            rss_get = rss_get.get('href')
+            return rss_get
+        else: return None
+
+    def get_atom(self):
+        atom_get = self.soup.find('link', type='application/atom+xml')
+        if atom_get:
+            atom_get = atom_get.get('href')
+            return atom_get
+        else: return None
+    def get_feed(self):
+        rss = self.get_rss()
+        atom = self.get_atom()
+        return rss, atom
